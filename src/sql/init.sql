af343f75e93b25ad9d0fb0937c4617d68aef1b00
William Redding <williamdredding@gmail.com>
2024-12-24 22:56:02 +0000

    Database indexes

--- src/sql/init.sql
+++ src/sql/init.sql
@@ -14,10 +14,16 @@ CREATE TABLE skins (
     FOREIGN KEY (owner_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
+CREATE INDEX idx_skins_owner_id ON skins (owner_id);
+CREATE INDEX idx_skins_owner_id_name ON skins (owner_id, name);
+
 CREATE TABLE stickers (
     id BIGSERIAL PRIMARY KEY,
     owner_id BIGINT NOT NULL,
     name TEXT NOT NULL,
     FOREIGN KEY (owner_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE INDEX idx_stickers_owner_id ON stickers (owner_id);
+CREATE INDEX idx_stickers_owner_id_name ON stickers (owner_id, name)
 
